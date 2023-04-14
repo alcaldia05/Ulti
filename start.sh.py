f2773866a805ca12e4175d3079fd5f6cb1bd0063
ARCADIA_PRJECT <117576633+alcaldia05@users.noreply.github.com>
2023-04-13 21:07:05 -0400

    Delete start.sh.py

--- start.sh.py
+++
@@ -1,6 +0,0 @@
-echo "Cloning Repo...."
-git clone https://github.com/alcaldia05/Ulti /Ulti
-cd /Ulti
-pip3 install -U -r requirements.txt
-echo "Starting Bot.... Please Wait. Check You Log. Bot Edit by @LISA_FAN_KL"
-python3 bot.py
